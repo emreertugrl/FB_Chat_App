7bf9131d784a121fe8d11d3469c6aa816cc3de2e
emreertugrl <emrertugrl@gmail.com>
2024-09-25 21:30:25 +0300

    login page, room page and chat page scss done, login page and room page dynamic code done

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { auth } from "../firebase";
 
-const ChatPage = () => {
-  return <div>ChatPage</div>;
+const ChatPage = ({ setIsAuth, setRoom, room }) => {
+  // todo mesajı veritabanına kaydet
+  return (
+    <div className="chat-page">
+      <header>
+        <p className="capitalize">{auth?.currentUser?.displayName}</p>
+        <div className="down">
+          <p className="capitalize">{room}</p>
+          <p>&#9660;</p>
+        </div>
+        <button onClick={() => setRoom(null)}>Farklı Oda</button>
+      </header>
+
+      <main>x</main>
+
+      <form className="message-form">
+        <input type="text" placeholder="Mesajınızı yazınız..." />
+        <button type="submit">Gönder</button>
+      </form>
+    </div>
+  );
 };
 
 export default ChatPage;
--- src/Pages/RoomPage.jsx
+++ src/Pages/RoomPage.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 
-const RoomPage = () => {
+const RoomPage = ({ setIsAuth, setRoom }) => {
+  // ! Çıkış yapınca
+  const logOut = () => {
+    // yetki state false yapılır
+    setIsAuth(false);
+    // local'deki tokeni sileriz
+    localStorage.removeItem("token");
+    // firebase bağlantısını kapat
+    signOut(auth);
+  };
+
+  // ! Form gönderilince
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // inputtaki girdiyi almak
+    const room = e.target[0].value.toLowerCase();
+
+    // sohbet sayfasına yönlendir
+    setRoom(room);
+  };
+
+  //
   return (
-    <div className="room-page">
+    <form onSubmit={handleSubmit} className="room-page">
       <h1>Chat Odası</h1>
       <p>Hangi Odaya Gireceksiniz ?</p>
 
       <input type="text" placeholder="ör:haftasonu" required />
 
-      <button>Odaya Gir</button>
-      <button>Çıkış Yap</button>
-    </div>
+      <button type="submit">Odaya Gir</button>
+      <button type="button" onClick={logOut}>
+        Çıkış Yap
+      </button>
+    </form>
   );
 };
 
